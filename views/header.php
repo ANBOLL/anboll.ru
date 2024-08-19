1f478221ca1ab2a34348d6ce7cf556ac7814b3ac
Болдырев Андрей <boldyrev@techart.ru>
2024-08-19 11:48:28 +0300

    Правка по кешу

--- views/header.php
+++ views/header.php
@@ -3,6 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="cache-control" content="no-cache, no-store, must-revalidate">
+    <meta http-equiv="pragma" content="no-cache">
+    <meta http-equiv="expires" content="0">
     <link type="image/x-icon" href="/images/site/favicon.ico" rel="shortcut icon">
     <link rel="stylesheet" href="/style/config.css">
     <link rel="stylesheet" href="/style/header.css">
